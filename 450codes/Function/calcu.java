54a6ad697d43df69228990746f58e9b782343f85
Piyushshind <piyushshinde.199@gmail.com>
2024-07-22 09:55:39 +0530

    priority

---
+++ 450codes/Function/calcu.java
@@ -0,0 +1,13 @@
+public class calcu {
+    public static void main(String[] args) {
+        String st = "hello" , s = "java";
+        int n1= 10 , n2=20;
+          System.out.println( n1+n2);
+          System.out.println( "n1+n2");
+          System.out.println( st + n1);
+          System.out.println( st+n1+n2);
+          System.out.println( n1+n2+st);
+          System.out.println(st+ n1*n2);
+          System.out.println( st+n1+n2+s);
+    }
+}
