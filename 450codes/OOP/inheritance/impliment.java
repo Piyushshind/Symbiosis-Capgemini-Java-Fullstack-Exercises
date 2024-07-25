be3bb677893e5faf1cc42004c05e86403e4bb97e
Piyushshind <piyushshinde.199@gmail.com>
2024-07-25 09:42:46 +0530

    inheritance

--- 450codes/OOP/inheritance/impliment.java
+++ 450codes/OOP/inheritance/impliment.java
@@ -1,11 +1,13 @@
 package inheritance;
 
 class s {
+    int a = 10;
     public void add(){
         System.out.println("from s class");
     }
 }
  class d extends s{
+    int b= 20;
     public void sd(){
         System.out.println(" from d class");
         
@@ -17,7 +19,8 @@ class s {
 class finall extends d implements tg {
         @Override
         public void pri() {
-        System.out.println("callled  ...... from interface");
+            int sum = a+b;
+        System.out.println("callled from interface and sum of A + B is :- " + sum);
             
         }
 
