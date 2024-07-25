b15067a1a1cfb055597cef87f14cfac9eada9058
Piyushshind <piyushshinde.199@gmail.com>
2024-07-25 10:17:15 +0530

    cal

---
+++ 450codes/Function/calu.java
@@ -0,0 +1,11 @@
+public class calu {
+    public static void main(String[] args) {
+        int num = 8*25/50/4*90*2/15+18*3-14*3-10;
+        if(++num> 30 && ++num <50 || ++num >25 && ++num <40){
+            System.out.println(num +"+");
+        }else {
+            System.out.println(num *2-4);
+        }
+        
+    }
+}
