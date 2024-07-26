f5ec8a36e50e4fb76b2cd6c2548ed03e70bf937c
Piyushshind <piyushshinde.199@gmail.com>
2024-07-26 09:51:34 +0530

    array list

---
+++ 450codes/Array/homogeniouse.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class homogeniouse {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        ArrayList ad = new ArrayList<>();
+        ad.add("Akshay");
+        ad.add(73.898);
+        ad.add("AJay");
+        ad.add(85);
+        ad.add("omkar");
+        ad.add(true);
+
+        // for (int i = 0; i < 5; i++) {
+        // int a = sc.nextInt();
+        // ad.add(a);
+        // }
+        // for (int i = 0; i < 5; i++) {
+
+        // System.out.println(ad.get(i));
+        // }
+   System.out.println(ad);
+    }
+}
--- 450codes/Function/calu.java
+++ 450codes/Function/calu.java
@@ -1,7 +1,7 @@
 public class calu {
     public static void main(String[] args) {
         int num = 8*25/50/4*90*2/15+18*3-14*3-10;
-        if(++num> 30 && ++num <50 || ++num >25 && ++num <40){
+        if(++num> 30 && ++num <50 || ++num <50 && ++num >20){
             System.out.println(num +"+");
         }else {
             System.out.println(num *2-4);
