4f7882f65853f319ad8b79a02993d6043db45bdf
Piyushshind <piyushshinde.199@gmail.com>
2024-07-27 09:18:49 +0530

    sorted arrayList

--- 450codes/Array/homogeniouse.java
+++ 450codes/Array/homogeniouse.java
@@ -1,24 +1,31 @@
 import java.util.*;
 
 public class homogeniouse {
+    public static void RemoveDuplicate(ArrayList<Integer> ad) {
+        ArrayList<Integer> sd = new ArrayList<>();
+
+        int temp = ad.get(0);
+        sd.add(temp);
+
+        for (int i = 0; i < ad.size(); i++) {
+            if (!ad.get(i).equals(temp)) {
+                sd.add(ad.get(i));
+                temp = ad.get(i);
+            }
+        }
+        System.out.println(sd);
+    }
+
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         ArrayList ad = new ArrayList<>();
-        ad.add("Akshay");
-        ad.add(73.898);
-        ad.add("AJay");
-        ad.add(85);
-        ad.add("omkar");
-        ad.add(true);
 
-        // for (int i = 0; i < 5; i++) {
-        // int a = sc.nextInt();
-        // ad.add(a);
-        // }
-        // for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
+            int a = sc.nextInt();
+            ad.add(a);
+        }
+        Collections.sort(ad);
 
-        // System.out.println(ad.get(i));
-        // }
-   System.out.println(ad);
+        RemoveDuplicate(ad);
     }
 }
