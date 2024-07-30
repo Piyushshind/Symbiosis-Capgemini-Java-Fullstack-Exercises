ec30b0a60ca58448aaaf45f34a2a84d577774af9
Piyushshind <piyushshinde.199@gmail.com>
2024-07-30 22:01:25 +0530

    Itterable

---
+++ 450codes/Array/Itterable.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class Itterable {
+    public static void main(String[] args) {
+        ArrayList arr = new ArrayList<>();
+          
+        Iterator as = arr.iterator() ;
+    }
+}
