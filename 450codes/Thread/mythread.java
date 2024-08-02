15a81de284f6d72dc42fbc7815be543830a7ea91
Piyushshind <piyushshinde.199@gmail.com>
2024-08-02 21:33:42 +0530

    thread

--- 450codes/Thread/mythread.java
+++ 450codes/Thread/mythread.java
@@ -3,18 +3,24 @@ class myTread implements Runnable {
     public int a = 0;
 
     public void run() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 50; i++) {
             long id = Thread.currentThread().getId();
+            String name = Thread.currentThread().getName();
             if (id == 21) {
-                System.out.println("t1 thread is running with id :- " + id + " and connt " + (++a) + " Even "
-                        + "with Loop-index :- " + i);
+                if (i % 2 == 0) {
+                    System.out
+                            .println(name + " t1 thread is running with id :- " + id + " and connt " + (++a) 
+                                    + "with Loop-index :- " + i);
+                }
             }
             if (id == 22) {
-                System.out.println("t2 thread is running with id :- " + id + " and connt " + (++a) + " Odd "
-                        + "with Loop-index :- " + i);
+                if (i % 2 != 0) {
+                    System.out.println(name + " t2 thread is running with id :- " + id + " and connt " + (++a) + " Odd "
+                            + "with Loop-index :- " + i);
+                }
             }
             if (id == 23) {
-                System.out.println("t3 thread is running with id :- " + id + " and connt " + (++a) + " Nutral"
+                System.out.println(name + " t3 thread is running with id :- " + id + " and connt " + (++a) + " Nutral"
                         + " with Loop-index :- " + i);
             }
         }
@@ -28,6 +34,10 @@ public class mythread {
         Thread t1 = new Thread(s1);
         Thread t2 = new Thread(s1);
         Thread t3 = new Thread(s1);
+
+        t1.setName("Fisrt");
+        t2.setName("Second");
+        t3.setName("Third");
 
         t1.setPriority(5);
         t2.setPriority(1);
