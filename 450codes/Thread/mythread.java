7d04961d31a3bc23aebe95728c9f773201cc3573
Piyushshind <piyushshinde.199@gmail.com>
2024-08-02 09:35:51 +0530

    thread

--- 450codes/Thread/mythread.java
+++ 450codes/Thread/mythread.java
@@ -1,10 +1,11 @@
 // package 450codes.Thread;
 class myTread implements Runnable {
-    public static int a = 0;
+    public  int a = 0;
 
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getId() + " is running ...." + (++a) + " times ");
+            System.out.println("Thread " + Thread.currentThread().getId() + " is running ...." + (++a) + " times "
+                    + "with Loop-index :- " + i);
         }
     }
 }
@@ -16,7 +17,7 @@ public class mythread {
         Thread t1 = new Thread(s1);
         Thread t2 = new Thread(s1);
         Thread t3 = new Thread(s1);
-        
+
         t1.setPriority(5);
         t2.setPriority(1);
         t3.setPriority(10);
