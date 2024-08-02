9dd55f15be387b999c50ac6417f4e707130bfcc9
Piyushshind <piyushshinde.199@gmail.com>
2024-08-02 09:15:54 +0530

    thread priority

--- 450codes/Thread/mythread.java
+++ 450codes/Thread/mythread.java
@@ -1,7 +1,26 @@
 // package 450codes.Thread;
+class myTread implements Runnable {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " is running ...." + i + " times ");
+
+        }
+    }
+}
 
 public class mythread {
+
     public static void main(String[] args) {
-          
+        myTread s1 = new myTread();
+        Thread t1 = new Thread(s1);
+        Thread t2 = new Thread(s1);
+        Thread t3 = new Thread(s1);
+        t1.setPriority(5);
+        t2.setPriority(1);
+        t3.setPriority(10);
+        t1.start();
+        t2.start();
+        t3.start();
+
     }
 }
