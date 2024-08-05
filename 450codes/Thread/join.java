c600820616f1b14f9da3316887e449728a4f7d70
Piyushshind <piyushshinde.199@gmail.com>
2024-08-05 09:43:36 +0530

    java thread

---
+++ 450codes/Thread/join.java
@@ -0,0 +1,30 @@
+
+class ChildThread extends Thread {
+    public void run() {
+        System.out.println("Child thread is running...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println("Child thread interrupted.");
+        }
+        System.out.println("Child thread has finished.");
+    }
+}
+
+public class join {
+    public static void main(String[] args) {
+        ChildThread childThread = new ChildThread();
+        childThread.start();
+
+        try {
+            childThread.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+
+        System.out.println("Main thread resumes after child thread has finished.");
+    }
+}
+
+
+
