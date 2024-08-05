b671441d0c4b78feade0ff7aacd519259a6e01e5
Piyushshind <piyushshinde.199@gmail.com>
2024-08-05 10:21:20 +0530

    main thread

---
+++ 450codes/Thread/mainthread.java
@@ -0,0 +1,27 @@
+
+public class mainthread {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + " main thred is running ....");
+        try{
+            Thread.sleep(2000);
+        }catch(Exception e){
+            System.out.println( e);
+        }
+        System.out.println(" main thread has stoped ..");
+        newThread np = new newThread();
+        np.start();
+        try {
+            np.join();
+            // Thread.currentThread().join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+       
+    }
+}
+
+class newThread extends Thread {
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " is running ......");
+    }
+}
