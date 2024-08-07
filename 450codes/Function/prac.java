6851d4f45465417612916fe6fc6b33a696acefd3
Piyushshind <piyushshinde.199@gmail.com>
2024-08-07 09:06:27 +0530

    $ variable

--- 450codes/Function/prac.java
+++ 450codes/Function/prac.java
@@ -1,9 +1,11 @@
 public class prac {
     public static void main(String[] args) {
         int sum =0;
+        int $_ = 5; // no error while declairing $
         for (int i = 0 ,j=0; i < 5 & j<5; ++i,j=i+1) {
              sum+=i;
              System.out.println( sum);
         }
+
     }
 }
