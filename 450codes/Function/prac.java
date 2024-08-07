e14f1fd5ed83806126d1dc0f9028333b17abb994
Piyushshind <piyushshinde.199@gmail.com>
2024-08-07 09:18:05 +0530

    practice

--- 450codes/Function/prac.java
+++ 450codes/Function/prac.java
@@ -1,10 +1,11 @@
 public class prac {
     public static void main(String[] args) {
-        int sum =0;
+        Integer sum = 01;
+       
         int $_ = 5; // no error while declairing $
-        for (int i = 0 ,j=0; i < 5 & j<5; ++i,j=i+1) {
-             sum+=i;
-             System.out.println( sum);
+        for (int i = 0, j = 0; i < 5 & j < 5; ++i, j = i + 1) {
+            sum += i;
+            System.out.println(sum);
         }
 
     }
