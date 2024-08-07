15f1714911de4cf670c7046b7c84417723d8b421
Piyushshind <piyushshinde.199@gmail.com>
2024-08-07 10:12:34 +0530

    dynamic your printing

--- 450codes/File/outputstrim.java
+++ 450codes/File/outputstrim.java
@@ -6,14 +6,27 @@ public class outputstrim {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         String ClgName = sc.nextLine();
+        sc.close();
 
         try {
             FileOutputStream f = new FileOutputStream("E:\\\\Notapad/dummy.txt", true);
-            String str = " and he is studdying MCA ";
+            byte bytName[] = name.getBytes();
+            byte bytclgName[] = ClgName.getBytes();
+
+            String str = "Student name is :- ";
+            String str2 = "And he is studdying in collage :- ";
             char ch[] = str.toCharArray();
+            char ch1[] = str2.toCharArray();
             for (int i = 0; i < str.length(); i++) {
                 f.write(ch[i]);
             }
+            f.write(bytName);
+            f.write('\n');
+            for (int i = 0; i < str2.length(); i++) {
+                f.write(ch1[i]);
+            }
+            f.write(bytclgName);
+
             System.out.println("file Updated succefully ");
             f.close();
         } catch (Exception e) {
