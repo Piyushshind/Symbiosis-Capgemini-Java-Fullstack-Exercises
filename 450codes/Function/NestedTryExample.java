725856a30094103425e90ce18acd13651a00d729
Piyushshind <piyushshinde.199@gmail.com>
2024-08-10 09:51:53 +0530

    nested try block

---
+++ 450codes/Function/NestedTryExample.java
@@ -0,0 +1,21 @@
+public class NestedTryExample {
+    public static void main(String[] args) {
+        try {
+            int[] numbers = {1, 2, 3};
+            System.out.println("Accessing element at index 5: " + numbers[5]); 
+            
+            try {
+                int result = numbers[2] / 0; 
+                System.out.println("Result of division: " + result);
+            } catch (ArithmeticException e) {
+                System.out.println("ArithmeticException caught in inner try block: " + e.getMessage());
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException caught in outer try block: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("General exception caught in outer try block: " + e.getMessage());
+        }
+        
+        System.out.println("Program continues...");
+    }
+}
