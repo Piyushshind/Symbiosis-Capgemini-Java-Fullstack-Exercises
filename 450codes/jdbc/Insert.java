f20a183736605ab64969f39140ba014265436746
Piyushshind <piyushshinde.199@gmail.com>
2024-09-02 20:40:25 +0530

    insert

--- 450codes/jdbc/Insert.java
+++ 450codes/jdbc/Insert.java
@@ -1,47 +1,48 @@
 package myjdbc;
+
 import java.sql.*;
 
 public class Insert {
-	
+
 	public void createTable() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-		       Driver myDriver = new com.mysql.jdbc.Driver();
-		       DriverManager.registerDriver(myDriver);
-		       //String url = "jdbc:mysql://localhost:3306/students";
-		       //String UserName = "root";
-		      // String Password = "root";
-		       Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/students","root","root") ;        
-		       Statement statement = con.createStatement();
-		       
-		       String MyQueryString = "Create table if not exists student1(name varchar(30),age int, address varchar(30) )";
-		       
-		       statement.executeUpdate(MyQueryString);
-		       System.out.println("table created successfuly");
-		       con.close();
+			Driver myDriver = new com.mysql.jdbc.Driver();
+			DriverManager.registerDriver(myDriver);
+			// String url = "jdbc:mysql://localhost:3306/students";
+			// String UserName = "root";
+			// String Password = "root";
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/students", "root", "root");
+			Statement statement = con.createStatement();
 
-		}catch(Exception e){
+			String MyQueryString = "Create table if not exists student1(name varchar(30),age int, address varchar(30) )";
+
+			statement.executeUpdate(MyQueryString);
+			System.out.println("table created successfuly");
+			con.close();
+
+		} catch (Exception e) {
 			System.out.println("Exe " + e);
 		}
-		
+
 	}
-	
+
 	public void insertData() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-		     Driver myDriver = new com.mysql.jdbc.Driver();
-		     DriverManager.registerDriver(myDriver);
-		       String url = "jdbc:mysql://localhost:3306/students";
-		       String UserName = "root";
-		       String Password = "root";
-		       Connection con = DriverManager.getConnection(url, UserName, Password) ;        
-		       Statement stmt = con.createStatement();
-		       String query = "Insert into student1 values('Piyush',23,'Hirawadi Panchavati')";
-		       stmt.executeUpdate(query);
-		       System.out.println("Data inserted successfuly");
-		       con.close();   
-			
-		}catch(Exception e ) {
+			Driver myDriver = new com.mysql.jdbc.Driver();
+			DriverManager.registerDriver(myDriver);
+			String url = "jdbc:mysql://localhost:3306/students";
+			String UserName = "root";
+			String Password = "root";
+			Connection con = DriverManager.getConnection(url, UserName, Password);
+			Statement stmt = con.createStatement();
+			String query = "Insert into student1 values('Piyush',23,'Hirawadi Panchavati')";
+			stmt.executeUpdate(query);
+			System.out.println("Data inserted successfuly");
+			con.close();
+
+		} catch (Exception e) {
 			System.out.println("Exe " + e);
 		}
 	}
